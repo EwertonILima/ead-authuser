9ad7215301b22fe570226b9f20f234845f44c586
ewertonilima <ewertoninaciolima@gmail.com>
2024-03-06 18:35:15 -0300

    :sparkles: Implements retry with Resilience4j
    🔧 Add Timeout
    ➕ Add Circuitbreaker Resilience4j dependency
    ➕ Add Spring Actuator dependency

--- src/main/java/com/ewertonilima/authuser/configs/RestTemplateConfig.java
+++ src/main/java/com/ewertonilima/authuser/configs/RestTemplateConfig.java
@@ -6,13 +6,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+
 @Configuration
 public class RestTemplateConfig {
+
+    static final int TIMEOUT = 5000;
 
     @LoadBalanced
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         // Do any additional configuration here
-        return builder.build();
+        return builder
+                .setConnectTimeout(Duration.ofMillis(TIMEOUT))
+                .setReadTimeout(Duration.ofMillis(TIMEOUT))
+                .build();
     }
 }
