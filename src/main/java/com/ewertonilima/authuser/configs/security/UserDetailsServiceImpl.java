85bec758c0bea32020773e19084a66eccd4f5d6e
ewertonilima <ewertoninaciolima@gmail.com>
2024-03-26 20:10:35 -0300

    feat: :sparkles: Implements authentication  with UserDetails and UserDetailsService
    Using basic authentication with Database

---
+++ src/main/java/com/ewertonilima/authuser/configs/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.ewertonilima.authuser.configs.security;
+
+import com.ewertonilima.authuser.models.UserModel;
+import com.ewertonilima.authuser.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserModel userModel = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+        return UserDetailsImpl.build(userModel);
+    }
+}
--- src/main/java/com/ewertonilima/authuser/configs/security/WebSecurityConfig.java
+++ src/main/java/com/ewertonilima/authuser/configs/security/WebSecurityConfig.java
@@ -1,7 +1,8 @@
-package com.ewertonilima.authuser.configs;
+package com.ewertonilima.authuser.configs.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,6 +16,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    final UserDetailsServiceImpl userDetailsService;
+    final AuthenticationEntryPointImpl authenticationEntryPoint;
+
+    public WebSecurityConfig(UserDetailsServiceImpl userDetailsService, AuthenticationEntryPointImpl authenticationEntryPoint) {
+        this.userDetailsService = userDetailsService;
+        this.authenticationEntryPoint = authenticationEntryPoint;
+    }
+
     private static final String[] AUTH_WHITELIST = {
             "/auth/**"
     };
@@ -22,21 +31,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .httpBasic()
-            .and()
-            .authorizeRequests()
-            .antMatchers(AUTH_WHITELIST).permitAll()
-            .anyRequest().authenticated()
-            .and()
-            .csrf().disable();
+                .httpBasic()
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .and()
+                .authorizeRequests()
+                .antMatchers(AUTH_WHITELIST).permitAll()
+                .antMatchers(HttpMethod.GET, "/users/**").hasRole("STUDENT")
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("admin")
-                .password(passwordEncoder().encode("123456"))
-                .roles("ADMIN");
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
     @Bean
