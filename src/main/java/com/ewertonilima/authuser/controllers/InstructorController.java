10e6c08f54ab11d6d7326ab4625913037543b033
EwertonILima <ewertoninaciolima@gmail.com>
2024-04-20 20:17:25 -0300

    feat: :sparkles: Add authorization role to Instructor

--- src/main/java/com/ewertonilima/authuser/controllers/InstructorController.java
+++ src/main/java/com/ewertonilima/authuser/controllers/InstructorController.java
@@ -1,16 +1,16 @@
 package com.ewertonilima.authuser.controllers;
 
 import com.ewertonilima.authuser.dtos.InstructorDto;
+import com.ewertonilima.authuser.enums.RoleType;
 import com.ewertonilima.authuser.enums.UserType;
+import com.ewertonilima.authuser.models.RoleModel;
 import com.ewertonilima.authuser.models.UserModel;
+import com.ewertonilima.authuser.service.RoleService;
 import com.ewertonilima.authuser.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.time.LocalDateTime;
@@ -23,21 +23,29 @@ import java.util.Optional;
 public class InstructorController {
 
     final UserService userService;
+    final RoleService roleService;
 
-    public InstructorController(UserService userService) {
+    public InstructorController(UserService userService, RoleService roleService) {
         this.userService = userService;
+        this.roleService = roleService;
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping("/subscription")
     public ResponseEntity<Object> saveSubscriptionInstructor(@RequestBody @Valid InstructorDto instructorDto) {
         Optional<UserModel> userModelOptional = userService.findById(instructorDto.getUserId());
         if (!userModelOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User Not Found.");
+        } else {
+            RoleModel roleModel = roleService.findByRoleName(RoleType.ROLE_INSTRUCTOR)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is Not Found"));
+
+            var userModel = userModelOptional.get();
+            userModel.setUserType(UserType.INSTRUCTOR);
+            userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+            userModel.getRoles().add(roleModel);
+            userService.updateUser(userModel);
+            return ResponseEntity.status(HttpStatus.CREATED).body(userModel);
         }
-        var userModel = userModelOptional.get();
-        userModel.setUserType(UserType.INSTRUCTOR);
-        userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
-        userService.updateUser(userModel);
-        return ResponseEntity.status(HttpStatus.CREATED).body(userModel);
     }
 }
